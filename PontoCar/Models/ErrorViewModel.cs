ebe8629d6024e904740b054e1b8c08cb908bd60c
Renan <renan.automation@gmail.com>
2023-04-05 12:38:54 -0300

    gitignore

---
+++ PontoCar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PontoCar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
